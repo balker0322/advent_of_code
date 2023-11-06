318eaf16230105b04373f3ded8b3ccc3dc4da311
balker0322 <bryogenin_haul@aleeas.com>
2023-11-06 21:15:34 +0800

    adding solution for 2015 day 9 part 1

--- 2015_day_09/part1.py
+++ 2015_day_09/part1.py
@@ -7,23 +7,55 @@ def main():
     input_lines = get_input_lines()
 
     graph = {}
-    loc = set([])
+    loc_list = set([])
     for line in input_lines:
         src, _, dst, _, dist = line.split()
-        graph[(src, dst)] = int(dist)
-        graph[(dst, src)] = int(dist)
-        loc.add(src)
-        loc.add(dst)
-    
+        if not src in graph.keys():
+            graph[src] = {}
+        if not dst in graph.keys():
+            graph[dst] = {}
+        graph[src][dst] =  int(dist)
+        graph[dst][src] =  int(dist)
+        loc_list.add(src)
+        loc_list.add(dst)
+
+    loc = {l:i for i, l in enumerate(loc_list)}
+
     q = deque()
+    q.append((''.join(['0' for _ in loc]), None, 0))
     visited = set([])
-    q.add(set([]))
+    min_dist = float('inf')
 
-    # while q:
+    while q:
 
-    #     for n in 
+        curr_node = q.pop()
+        vmark, curr, td = curr_node
+
+        if curr_node in visited:
+            continue
+        
+        visited.add((vmark, curr, td))
+
+        if not curr:
+            for l in loc.keys():
+                q.append((vmark, l, td))
+            continue
+
+        nvmark = list(vmark)
+        nvmark[loc[curr]] = '1'
+        nvmark = ''.join(nvmark)
+
+        if sum([int(m) for m in nvmark]) == len(loc_list):
+            min_dist = min(min_dist, td)
+            continue
+
+        for nloc, d in graph[curr].items():
+            mark = loc[nloc]
+            if vmark[mark] == '1':
+                continue
+            q.append((nvmark, nloc, td+d))
     
-
+    print(min_dist)
 
 if __name__=='__main__':
     main()
