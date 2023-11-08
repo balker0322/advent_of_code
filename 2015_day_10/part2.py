a13275f47616267bbd6fc9c7109315d5c490317e
balker0322 <bryogenin_haul@aleeas.com>
2023-11-08 17:26:27 +0800

    adding solution

---
+++ 2015_day_10/part2.py
@@ -0,0 +1,21 @@
+from itertools import groupby
+
+
+def look_and_say(num:str)->str:
+    r = [str(len(list(g))) + k for k, g in groupby(num)]
+    return ''.join(r)
+
+
+def main():
+    input_num = '1'
+    for _ in range(4):
+        input_num = look_and_say(input_num)
+    print(input_num)
+
+    input_num = '1113122113'
+    for _ in range(50):
+        input_num = look_and_say(input_num)
+    print(len(input_num))
+
+if __name__=='__main__':
+    main()
